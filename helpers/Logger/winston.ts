03d4fb74089988655fb79d5a19bd2e1ce75974d3
Donald Moore <donaldwaynemoorejr@gmail.com>
2024-03-11 21:35:08 -0500

    fix: Winston app name

--- helpers/Logger/winston.ts
+++ helpers/Logger/winston.ts
@@ -2,7 +2,7 @@ import { createLogger, format, transports } from "winston";
 
 const httpTransportOptions = {
   host: "http-intake.logs.datadoghq.com",
-  path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=NO_NAME`,
+  path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=YOUR_APP_NAME_HERE`,
   ssl: true
 };
 
